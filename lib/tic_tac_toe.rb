135b74807c13f096e86ff0eec8d70073544ade66
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-04-23 17:55:53 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,6 +39,6 @@ def position_taken?(board, position)
 end
 
 def valid_move?(board, position)
-   board[position] && !position_taken?(board, position) 
+   board[position] && !position_taken?(board, position)
 
 end
