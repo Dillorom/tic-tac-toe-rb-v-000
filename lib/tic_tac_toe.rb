94f18e4af08470847efd8b23d6689557a2af31c7
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-04-23 17:04:00 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,6 +10,7 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [6,4,2]
 ]
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -18,3 +19,8 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 display_board(board)
+
+def input_to_index(board)
+  input = gets.strip
+  index = input.to_i - 1
+end
