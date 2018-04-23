cae9088c3c56db96fc71f9ce30d3c935935be8e6
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-04-23 17:13:34 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -24,6 +24,6 @@ def input_to_index(input)
   input.to_i - 1
 end
 
-def move(board, index, character)
-  index = input_to_index(input)
+def move(board, position, player_token)
+  position = input_to_index(input)
 end
